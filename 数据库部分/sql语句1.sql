76884073ed08e916538471563d39a847085121dc
hjl <hjl759835840@163.com>
2018-08-02 18:11:05 +0800

    加了个用户评分的表

--- 数据库部分/sql语句1.sql
+++ 数据库部分/sql语句1.sql
@@ -1,40 +1,40 @@
 INSERT INTO director 
-(director_name,director_sex,director_age,director_country) 
+(director_name,director_sex,director_birth,director_country) 
 VALUES
 ('弗兰克·德拉邦特',1,'1959-01-28','法国');
 
 INSERT INTO director 
-(director_name,director_sex,director_age,director_country) 
+(director_name,director_sex,director_birth,director_country) 
 VALUES
 ('陈凯歌',1,'1952-08-12','中国');
 
 INSERT INTO director 
-(director_name,director_sex,director_age,director_country) 
+(director_name,director_sex,director_birth,director_country) 
 VALUES
 ('吕克·贝松',1,'1959-01-28','法国');
 
 INSERT INTO director 
-(director_name,director_sex,director_age,director_country) 
+(director_name,director_sex,director_birth,director_country) 
 VALUES
 ('罗伯特·泽米吉斯',1,'1952-05-14','美国');
 
 INSERT INTO director 
-(director_name,director_sex,director_age,director_country) 
+(director_name,director_sex,director_birth,director_country) 
 VALUES
 ('罗伯托·贝尼尼',1,'1952-10-27','意大利');
 
 INSERT INTO director 
-(director_name,director_sex,director_age,director_country) 
+(director_name,director_sex,director_birth,director_country) 
 VALUES
 ('詹姆斯·卡梅隆',1,'1954-08-16','加拿大');
 
 INSERT INTO director 
-(director_name,director_sex,director_age,director_country) 
+(director_name,director_sex,director_birth,director_country) 
 VALUES
 ('宫崎骏',1,'1941-01-05','日本');
 
 INSERT INTO director 
-(director_name,director_sex,director_age,director_country) 
+(director_name,director_sex,director_birth,director_country) 
 VALUES
 ('史蒂文·斯皮尔伯格',1,'1946-12-18','美国');
 
@@ -85,149 +85,149 @@ VALUES
 
 -- actor
 INSERT INTO actor 
-(actor_name,actor_sex,actor_age,actor_country) 
+(actor_name,actor_sex,actor_birth,actor_country) 
 VALUES
 ('蒂姆·罗宾斯',1,'1958-10-16','美国');
 
 INSERT INTO actor 
-(actor_name,actor_sex,actor_age,actor_country)
+(actor_name,actor_sex,actor_birth,actor_country)
 VALUES
 ('摩根·弗里曼',1,'1937-06-01','美国');
 
 INSERT INTO actor 
-(actor_name,actor_sex,actor_age,actor_country)
+(actor_name,actor_sex,actor_birth,actor_country)
 VALUES
 ('张国荣',1,'1956-09-12','中国香港');
 
 INSERT INTO actor 
-(actor_name,actor_sex,actor_age,actor_country)
+(actor_name,actor_sex,actor_birth,actor_country)
 VALUES
 ('张丰毅',1,'1956-09-01','中国');
 
 INSERT INTO actor 
-(actor_name,actor_sex,actor_age,actor_country)
+(actor_name,actor_sex,actor_birth,actor_country)
 VALUES
 ('让·雷诺',1,'1948-07-30','摩洛哥');
 
 INSERT INTO actor 
-(actor_name,actor_sex,actor_age,actor_country)
+(actor_name,actor_sex,actor_birth,actor_country)
 VALUES
 ('娜塔莉·波特曼',0,'1981-06-09','以色列');
 
 INSERT INTO actor 
-(actor_name,actor_sex,actor_age,actor_country)
+(actor_name,actor_sex,actor_birth,actor_country)
 VALUES
 ('汤姆·汉克斯',1,'1956-07-09','美国');
 
 INSERT INTO actor 
-(actor_name,actor_sex,actor_age,actor_country)
+(actor_name,actor_sex,actor_birth,actor_country)
 VALUES
 ('罗宾·怀特',0,'1966-04-08','美国');
 
 INSERT INTO actor 
-(actor_name,actor_sex,actor_age,actor_country) 
+(actor_name,actor_sex,actor_birth,actor_country) 
 VALUES
 ('罗伯托·贝尼尼',1,'1952-10-27','意大利');
 
 INSERT INTO actor 
-(actor_name,actor_sex,actor_age,actor_country)
+(actor_name,actor_sex,actor_birth,actor_country)
 VALUES
 ('尼可莱塔·布拉斯基',0,'1960-04-19','意大利');
 
 INSERT INTO actor 
-(actor_name,actor_sex,actor_age,actor_country)
+(actor_name,actor_sex,actor_birth,actor_country)
 VALUES
 ('莱昂纳多·迪卡普里奥',1,'1974-11-11','美国');
 
 INSERT INTO actor 
-(actor_name,actor_sex,actor_age,actor_country)
+(actor_name,actor_sex,actor_birth,actor_country)
 VALUES
 ('凯特·温丝莱特',0,'1975-10-05','英国');
 
 INSERT INTO actor 
-(actor_name,actor_sex,actor_age,actor_country)
+(actor_name,actor_sex,actor_birth,actor_country)
 VALUES
 ('柊瑠美',0,'1987-08-01','日本');
 
 INSERT INTO actor 
-(actor_name,actor_sex,actor_age,actor_country)
+(actor_name,actor_sex,actor_birth,actor_country)
 VALUES
 ('入野自由',1,'1988-02-19','日本');
 
 INSERT INTO actor 
-(actor_name,actor_sex,actor_age,actor_country)
+(actor_name,actor_sex,actor_birth,actor_country)
 VALUES
 ('连姆·尼森',1,'1952-06-07','英国');
 
 INSERT INTO actor 
-(actor_name,actor_sex,actor_age,actor_country)
+(actor_name,actor_sex,actor_birth,actor_country)
 VALUES
 ('本·金斯利',1,'1943-12-31','英国');
 
 
--- movieitem
+-- movie_item
 
-INSERT INTO movieitem
+INSERT INTO movie_item
 (movie_id,actor_id)
 VALUES
 (1,1);
-INSERT INTO movieitem
+INSERT INTO movie_item
 (movie_id,actor_id)
 VALUES
 (1,2);
-INSERT INTO movieitem
+INSERT INTO movie_item
 (movie_id,actor_id)
 VALUES
 (2,3);
-INSERT INTO movieitem
+INSERT INTO movie_item
 (movie_id,actor_id)
 VALUES
 (2,4);
-INSERT INTO movieitem
+INSERT INTO movie_item
 (movie_id,actor_id)
 VALUES
 (3,5);
-INSERT INTO movieitem
+INSERT INTO movie_item
 (movie_id,actor_id)
 VALUES
 (3,6);
-INSERT INTO movieitem
+INSERT INTO movie_item
 (movie_id,actor_id)
 VALUES
 (4,7);
-INSERT INTO movieitem
+INSERT INTO movie_item
 (movie_id,actor_id)
 VALUES
 (4,8);
-INSERT INTO movieitem
+INSERT INTO movie_item
 (movie_id,actor_id)
 VALUES
 (5,9);
-INSERT INTO movieitem
+INSERT INTO movie_item
 (movie_id,actor_id)
 VALUES
 (5,10);
-INSERT INTO movieitem
+INSERT INTO movie_item
 (movie_id,actor_id)
 VALUES
 (6,11);
-INSERT INTO movieitem
+INSERT INTO movie_item
 (movie_id,actor_id)
 VALUES
 (6,12);
-INSERT INTO movieitem
+INSERT INTO movie_item
 (movie_id,actor_id)
 VALUES
 (7,13);
-INSERT INTO movieitem
+INSERT INTO movie_item
 (movie_id,actor_id)
 VALUES
 (7,14);
-INSERT INTO movieitem
+INSERT INTO movie_item
 (movie_id,actor_id)
 VALUES
 (8,15);
-INSERT INTO movieitem
+INSERT INTO movie_item
 (movie_id,actor_id)
 VALUES
 (8,16);
