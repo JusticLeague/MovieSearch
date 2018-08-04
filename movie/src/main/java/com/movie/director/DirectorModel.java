9aed8e8a77326b5f7ae3bf92cbf9363fbd4c0f9d
HigherPool <546619475@qq.com>
2018-08-04 09:58:09 +0800

    加了些model

--- movie/src/main/java/com/movie/director/DirectorModel.java
+++ movie/src/main/java/com/movie/director/DirectorModel.java
@@ -1,5 +1,66 @@
 package com.movie.director;
 
+/**
+ * 导演的名称、性别、生日、国籍、简介、图片
+ * 
+ * @author QQ546
+ *
+ */
 public class DirectorModel {
+
+	String name;
+	int sex;
+	String birth;
+	String country;
+	String desc;
+	String pic;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSex() {
+		return sex;
+	}
+
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getPic() {
+		return pic;
+	}
+
+	public void setPic(String pic) {
+		this.pic = pic;
+	}
 
 }
---
+++ movie/src/main/java/com/movie/user/UserModel.java
@@ -0,0 +1,49 @@
+package com.movie.user;
+
+/**
+ * 会员的基本信息
+ *  头像、名称、账号、密码(MD5加密)
+ * 
+ * @author QQ546
+ *
+ */
+public class UserModel {
+
+	String pic;
+	String name;
+	String phone;
+	String password;
+
+	public String getPic() {
+		return pic;
+	}
+
+	public void setPic(String pic) {
+		this.pic = pic;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
