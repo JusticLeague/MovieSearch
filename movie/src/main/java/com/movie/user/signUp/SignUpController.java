1544e15e69e33eef8d97fb34f5434dd09005f390
HigherPool <546619475@qq.com>
2018-08-06 12:08:13 +0800

    修改

--- movie/src/main/java/com/movie/user/signUp/SignUpController.java
+++ movie/src/main/java/com/movie/user/signUp/SignUpController.java
@@ -9,27 +9,21 @@ import com.movie.user.User;
 
 /**
  * 注册控制器
+ * 
  * @author QQ546
  *
  */
 @Controller
 @RequestMapping("/signUp")
 public class SignUpController {
-	
+
 	@Autowired
 	SignUpService service;
-	
+
 	@PostMapping
-<<<<<<< HEAD
 	public String add(User u) {
 		service.addVip(u);
 		return "redirect:/signIn.html";
-=======
-	public String add(User user) {
-		service.addVip(user);
-		return "/signIn.html";
->>>>>>> b363fcd85cc158c9958430e0c3a6e1b1e262ad65
 	}
-	
 
 }
--- movie/src/main/java/com/movie/user/signUp/SignUpMapper.java
+++ movie/src/main/java/com/movie/user/signUp/SignUpMapper.java
@@ -9,11 +9,7 @@ import com.movie.user.User;
 public interface SignUpMapper {
 	
 	// 数据库vip表中插入数据
-<<<<<<< HEAD
-	@Insert("insert into vip(vip_name,vip_phone,vip_password,vip_pic) value(#{name}, #{phone}, #{password}, #{pic})")
-=======
 	@Insert("insert into vip(vip_name,vip_phone,vip_password,vip_pic) value(#{name}, #{phone}, #{password}, #{pic}")
->>>>>>> b363fcd85cc158c9958430e0c3a6e1b1e262ad65
 	void create(User user);
 
 }
